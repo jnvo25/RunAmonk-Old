e14b2bd60cf6259d9ff7040adf68867c283ba800
jonathanvo <jnvo25@gmail.com>
2022-03-10 15:31:58 -0800

    introduce second local player

--- assets/maps/scenes/homestage.js
+++ assets/maps/scenes/homestage.js
@@ -1,14 +1,26 @@
 var player;
+var player2;
 var cursors;
+
+let keyA;
+let keyS;
+let keyD;
+let keyW;
 export class HomeStage extends Phaser.Scene {
     constructor() {
         super('HomeStage');
     }
 
     preload() {
+        // Player 1 assets
         this.load.spritesheet('owlet-idle', 'assets/Owlet_Monster/Owlet_Monster_Idle_4.png', { frameWidth: 32, frameHeight: 32 });
         this.load.spritesheet('owlet-run', 'assets/Owlet_Monster/Owlet_Monster_Run_6.png', { frameWidth: 32, frameHeight: 32 });
         this.load.spritesheet('owlet-jump', 'assets/Owlet_Monster/Owlet_Monster_Jump_8.png', { frameWidth: 32, frameHeight: 32 });
+
+        // Player 2 assets
+        this.load.spritesheet('pinkie-idle', 'assets/Pink_Monster/Pink_Monster_Idle_4.png', { frameWidth: 32, frameHeight: 32 });
+        this.load.spritesheet('pinkie-run', 'assets/Pink_Monster/Pink_Monster_Run_6.png', { frameWidth: 32, frameHeight: 32 });
+        this.load.spritesheet('pinkie-jump', 'assets/Pink_Monster/Pink_Monster_Jump_8.png', { frameWidth: 32, frameHeight: 32 });
 
         this.load.image('background', 'assets/maps/images/background.png');
         this.load.image('spike', 'assets/maps/images/spike.png');
@@ -17,6 +29,12 @@ export class HomeStage extends Phaser.Scene {
     }
 
     create() {
+
+        keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+        keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+
         const backgroundImage = this.add.image(0,0, 'background').setOrigin(0,0);
         backgroundImage.setScale(2, 0.8);
 
@@ -25,8 +43,8 @@ export class HomeStage extends Phaser.Scene {
         const platforms = map.createStaticLayer('Platforms', tileset, 0, 0);
         platforms.setCollisionByExclusion(-1, true);
 
+        // Create player 1
         player = this.physics.add.sprite(100, 300, 'owlet-idle');
-
         player.setBounce(0.1);
         player.setCollideWorldBounds(true);
         this.physics.add.collider(player, platforms);
@@ -52,6 +70,33 @@ export class HomeStage extends Phaser.Scene {
             repeat: -1
         });
 
+        // Create player 2
+        player2 = this.physics.add.sprite(700, 300, 'pinkie-idle');
+        player2.setBounce(0.1);
+        player2.setCollideWorldBounds(true);
+        this.physics.add.collider(player2, platforms);
+
+        this.anims.create({
+            key: 'pinkie-idle',
+            frames: this.anims.generateFrameNumbers('pinkie-idle', {start: 0, end: 4}),
+            frameRate: 10,
+            repeat: -1
+        })
+
+        this.anims.create({
+            key: 'pinkie-run',
+            frames: this.anims.generateFrameNumbers('pinkie-run', {start: 0, end: 6}),
+            frameRate: 10,
+            repeat: -1
+        })
+
+        this.anims.create({
+            key: 'pinkie-jump',
+            frames: this.anims.generateFrameNumbers('pinkie-jump', {start: 0, end: 8}),
+            frameRate: 10,
+            repeat: -1
+        })
+
         cursors = this.input.keyboard.createCursorKeys();
 
     }
@@ -71,5 +116,21 @@ export class HomeStage extends Phaser.Scene {
             player.setVelocityX(0);
             player.anims.play('owlet-idle', true);
         }
+
+        if (keyW.isDown && player2.body.onFloor()) {
+            player2.setVelocityY(-400);
+        } else if (keyA.isDown) {
+            player2.setVelocityX(-160);
+            player2.setFlipX(true);
+            player2.anims.play('pinkie-run', true);
+        } else if (keyD.isDown) {
+            player2.setVelocityX(160);
+            player2.setFlipX(false);
+            player2.anims.play('pinkie-run', true);
+        } else {
+            player2.setVelocityX(0);
+            player2.anims.play('pinkie-idle', true);
+        }
+
     }
 }
